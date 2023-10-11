505cedaff81f4922d82400a69f1f8a500d2a1af1
avik012 <86908122+avik012@users.noreply.github.com>
2023-10-11 16:41:35 +0530

    Update auth.js console token

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,7 @@ const User = require('../models/userModel');
 exports.isAuthenticatedUser = catchAsyncError( async(req, res, next)=>{
     const {token}= req.cookies;
 
+    console.log("token",token)
     if(!token){
         return next(new ErrorHandler("Please login to access this resource",401)) 
     }
